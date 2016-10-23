5b031a811df27deb381e4e06dbb4fe34cf15ae45
Ian <ianh121@gmail.com>
2016-10-23 17:27:37 +0100

    Add a button to start/stop tracking; show toast if we move more than 10m

--- cycletrack/app/src/main/java/uk/co/zoobyware/cycletrack/MainActivity.java
+++ cycletrack/app/src/main/java/uk/co/zoobyware/cycletrack/MainActivity.java
@@ -1,30 +1,79 @@
 package uk.co.zoobyware.cycletrack;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
 
-public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, ActivityCompat.OnRequestPermissionsResultCallback {
 
     private GoogleApiClient googleApiClient;
+
+    private boolean tracking = false;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+
+    private Location previous = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Create an instance of GoogleAPIClient.
+        // Create an instance of GoogleAPIClient
         if (googleApiClient == null) {
             googleApiClient = new GoogleApiClient.Builder(this)
                     .addConnectionCallbacks(this)
                     .addOnConnectionFailedListener(this)
                     .addApi(LocationServices.API)
                     .build();
+        }
+
+        // Get the manager from the system
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        // Create the locationListener that we will be adding and removing
+        locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                recordLocation(location);
+            }
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+            public void onProviderEnabled(String provider) {}
+
+            public void onProviderDisabled(String provider) {}
+        };
+
+    }
+
+    public void recordLocation(Location loc) {
+        final float minDelta = 10;
+        float distance = 0;
+
+        if (previous != null) {
+            distance = loc.distanceTo(previous);
+        }
+
+        if (previous == null || distance >= minDelta) {
+            // TODO: store the location
+            Toast toast = Toast.makeText(getApplicationContext(), "You've moved " + distance + "m", Toast.LENGTH_SHORT);
+            toast.show();
+
+            previous = loc;
         }
     }
 
@@ -55,5 +104,38 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
+    }
+
+    public void toggleTracking(final View view) {
+        if (tracking) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            locationManager.removeUpdates(locationListener);
+
+            previous = null;
+
+            tracking = false;
+        } else {
+
+
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                final String[] locationPermissions = {Manifest.permission.ACCESS_FINE_LOCATION};
+
+                ActivityCompat.requestPermissions(this, locationPermissions, 0);
+                return;
+            }
+
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+            tracking = true;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == 0 && permissions[0].equals(Manifest.permission.ACCESS_FINE_LOCATION) && grantResults[0] == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+            tracking = true;
+        }
     }
 }
