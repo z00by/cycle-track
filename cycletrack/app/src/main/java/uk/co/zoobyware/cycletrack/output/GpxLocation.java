60d9601839745c649f3ea8a402f6c32d7e7e4d76
Ian <ianh121@gmail.com>
2017-12-31 18:52:48 +0000

    Format the date/time of locations explicitly so we always get seconds

--- cycletrack/app/src/main/java/uk/co/zoobyware/cycletrack/output/GpxLocation.java
+++ cycletrack/app/src/main/java/uk/co/zoobyware/cycletrack/output/GpxLocation.java
@@ -4,24 +4,26 @@ import android.location.Location;
 
 import org.threeten.bp.Instant;
 import org.threeten.bp.ZoneOffset;
+import org.threeten.bp.format.DateTimeFormatter;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
 public class GpxLocation {
     private final Location location;
 
+    private static final DateTimeFormatter iso8601Formatter = DateTimeFormatter.ISO_INSTANT;
+
     public GpxLocation (final Location location) {
         this.location = location;
     }
 
     public void write(final Document xml, final Element trackSegment) {
-
         final Element trackPoint = xml.createElement("trkpt");
         trackSegment.appendChild(trackPoint);
 
         final Element time = xml.createElement("time");
         trackPoint.appendChild(time);
-        time.appendChild(xml.createTextNode(Instant.ofEpochMilli(location.getTime()).atZone(ZoneOffset.UTC).toString()));
+        time.appendChild(xml.createTextNode(Instant.ofEpochMilli(location.getTime()).atZone(ZoneOffset.UTC).format(iso8601Formatter)));
 
         trackPoint.setAttribute("lat", Double.toString(location.getLatitude()));
         trackPoint.setAttribute("lon", Double.toString(location.getLongitude()));
